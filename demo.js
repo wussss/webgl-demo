4acf25304e3e87f6dcca80cab961909fe370e535
silan.wu <silan.wu@liulishuo.com>
2020-11-21 23:35:19 +0800

    feat:三角形

--- demo.js
+++ demo.js
@@ -1,20 +1,23 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('webgl'); //1、创建webgl画笔，可以调用webgl的API
+var canvas = document.getElementById('canvas')
+canvas.width = 100
+canvas.height = 100
+var ctx = canvas.getContext('webgl')//1、创建webgl画笔，可以调用webgl的API
 
-var program = ctx.createProgram(); //2、定义program,用于绑定顶点着色器和片元着色器
+var program = ctx.createProgram()//2、定义program,用于绑定顶点着色器和片元着色器
 
 //shader源代码
-var VSHADER_SOURCE =
-  'attribute vec4 a_Position;\n' + 
-  'void main () {\n' + 
-    'gl_Position = a_Position;\n' + 
-  '}\n'
+var VSHADER_SOURCE = `
+   attribute vec4 a_Position;
+   void main (){
+    gl_Position = a_Position;
+   }
+`
 
-var FSHADER_SOURCE =
-  'void main () {\n' + 
-    'gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' + 
-  '}\n'
-
+var FSHADER_SOURCE = `
+   void main(){
+    gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+   }
+`
 
 //3、声明顶点着色器和片元着色器
 function createShader(context, sourceCode, type) {
@@ -24,41 +27,44 @@ function createShader(context, sourceCode, type) {
     return shader
 }
 
+// define vertex shader
 var vertexShader = createShader(ctx, VSHADER_SOURCE, ctx.VERTEX_SHADER)
+// define frament shader
 var fragmentShader = createShader(ctx, FSHADER_SOURCE, ctx.FRAGMENT_SHADER)
 
 //4、着色器与program绑定
-ctx.attachShader(program, vertexShader);
-ctx.attachShader(program, fragmentShader);
+ctx.attachShader(program, vertexShader)
+ctx.attachShader(program, fragmentShader)
 //5、program与ctx绑定
-ctx.linkProgram(program); //5、绑定program
-ctx.useProgram(program); //6、使用program
+ctx.linkProgram(program)
+ctx.useProgram(program)
 ctx.program = program
 
-//6、初始化一个vertex buffer,将顶点位置传到vertex buffer中，最终传到vertexShader中
+//6、初始化一个vertex buffer,将点位置传到vertex buffer中，最终传到vertexShader中
 function initVertexBuffers(gl) {
-    var vertices = new Float32Array([
-        0, 0.5, -0.5, -0.5, 0.5, -0.5
-    ])
-    var n = 3
+    var vertices = new Float32Array([0, 0.5, 0, 0, 0.5, 0])//在画布中的顶点坐标的数组
     var vertexBuffer = gl.createBuffer()
+    var n = 3;
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
-    // write data into the buffer object
+    //将数据写入buffer
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
-    // get attribute a_Position address in vertex shader
+    //get顶点着色器里的变量a_Position
     var a_Position = gl.getAttribLocation(gl.program, 'a_Position')
+    //从vertices中每次拿两个数,赋值给a_Position
     gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0)
-    // enable a_Position variable
     gl.enableVertexAttribArray(a_Position)
-    return n
+    return n;
 }
-var n = initVertexBuffers(ctx)
 
-//7、绘制
+// 7、绘制
+//clear时设置背景色
 ctx.clearColor(0, 1, 0, 1)
-function render() {
+
+var n = initVertexBuffers(ctx)
+function draw() {
     ctx.clear(ctx.COLOR_BUFFER_BIT)
-    ctx.drawArrays(ctx.TRANGERS, 0, n)
+    initVertexBuffers(ctx)
+    ctx.drawArrays(ctx.TRIANGLES, 0, n)
 }
-render()
+draw()
 
