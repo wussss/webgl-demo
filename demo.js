d5b3b97eafbc25fc22b4a072fca46298a935bd2b
silan.wu <silan.wu@liulishuo.com>
2020-11-22 17:12:21 +0800

    feat:旋转

--- demo.js
+++ demo.js
@@ -1,75 +1,97 @@
 var canvas = document.getElementById('canvas')
-canvas.width = 200
-canvas.height = 200
-var ctx = canvas.getContext('webgl')//1、创建webgl画笔，可以调用webgl的API
+var gl = canvas.getContext('webgl')
 
-var program = ctx.createProgram()//2、定义program,用于绑定顶点着色器和片元着色器
+var program = gl.createProgram()
 
-//shader源代码
-var VSHADER_SOURCE = `
+var VSHADER_SOURCE, FSHADER_SOURCE
+
+VSHADER_SOURCE = `
    attribute vec4 a_Position;
-   void main (){
-    gl_Position = a_Position;
-   }
-`
-
-var FSHADER_SOURCE = `
+   uniform mat4 u_ModelMatrix;
    void main(){
-    gl_FragColor = vec4(0.84, 0.74, 0.88, 1);
+    gl_Position =  u_ModelMatrix * a_Position;
    }
 `
 
-//3、声明顶点着色器和片元着色器
-function createShader(context, sourceCode, type) {
-    var shader = context.createShader(type) //创建一个类型为type的着色器
-    context.shaderSource(shader, sourceCode) //绑定源代码
-    context.compileShader(shader)//编译源代码
+FSHADER_SOURCE = `
+   void main(){
+    gl_FragColor = vec4(0.84, 0.74, 0.88, 1);;
+   }
+`
+
+function createShader(gl, sourceCode, type) {
+    // create shader
+    var shader = gl.createShader(type)
+    gl.shaderSource(shader, sourceCode)
+    gl.compileShader(shader)
     return shader
 }
 
-var vertexShader = createShader(ctx, VSHADER_SOURCE, ctx.VERTEX_SHADER)
-var fragmentShader = createShader(ctx, FSHADER_SOURCE, ctx.FRAGMENT_SHADER)
+// define vertex shader
+var vertexShader = createShader(gl, VSHADER_SOURCE, gl.VERTEX_SHADER)
+// define frament shader
+var fragmentShader = createShader(gl, FSHADER_SOURCE, gl.FRAGMENT_SHADER)
 
-//4、着色器与program绑定
-ctx.attachShader(program, vertexShader)
-ctx.attachShader(program, fragmentShader)
-//5、program与ctx绑定
-ctx.linkProgram(program)/*  */
-ctx.useProgram(program)
-ctx.program = program
+// attach shader to program
+gl.attachShader(program, vertexShader)
+gl.attachShader(program, fragmentShader)
 
-//6、初始化一个vertex buffer,将点位置传到vertex buffer中，最终传到vertexShader中
+// link program to context
+gl.linkProgram(program)
+gl.useProgram(program)
+gl.program = program
+
+var currentAngle = 0
+var g_last = Date.now()
+
+
+
 function initVertexBuffers(gl) {
     var vertices = new Float32Array([
-        0, 1, 0, -1, 1, 0,
-        0, 1, 0, -1, -1, 0,
-        -0.5, 1, -0.5, 0.5, -1, 0.5,
-        0.5, 1, 0.5, 0.5, 1, 0.5,
-        -0.5, -1, -0.5, -0.5, -1, -0.5,
-        0.5, -1, 0.5, -0.5, 1, -0.5,
-    ])//在画布中的顶点坐标的数组，(0,0)在画布正中间,(0,1)在中间最上方
+        0, 0.5, -0.5, 0, 0.5, 0,
+        0, -0.5, -0.5, 0, 0.5, 0
+    ])
+    var n = vertices.length / 2
     var vertexBuffer = gl.createBuffer()
-    var n = vertices.length / 2;
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
-    //将数据写入buffer
+    // write data into the buffer object
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
-    //get顶点着色器里的变量a_Position
+    // get attribute a_Position address in vertex shader
     var a_Position = gl.getAttribLocation(gl.program, 'a_Position')
-    //从vertices中每次拿两个数,赋值给a_Position
     gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0)
+    // enable a_Position variable
     gl.enableVertexAttribArray(a_Position)
-    return n;
+    return n
 }
 
-// 7、绘制
-//clear时设置背景色
-ctx.clearColor(1,1,1,1);
+// write the positions of vertices to a vertex shader
+var n = initVertexBuffers(gl)
 
-var n = initVertexBuffers(ctx)
+var u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix')
+var modelMatrix = new Matrix4()
+
+
+function animate() {
+    var now = Date.now()
+    var duration = now - g_last
+    g_last = now
+    currentAngle = currentAngle + duration / 1000 * 180
+}
+gl.clearColor(1, 1, 1, 1)
 function draw() {
-    ctx.clear(ctx.COLOR_BUFFER_BIT)
-    initVertexBuffers(ctx)
-    ctx.drawArrays(ctx.TRIANGLES, 0, n)
+    modelMatrix.setRotate(currentAngle, 0, 1, 0)
+    //write postion data
+    gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements)
+    // clear canvas and add background color
+    gl.clear(gl.COLOR_BUFFER_BIT)
+    gl.drawArrays(gl.TRIANGLES, 0, n)
 }
-draw()
+var tick = function () {
+    // update the new rotation angle
+    animate()
+    // draw
+    draw()
+    requestAnimationFrame(tick)
+}
+tick()
 
