069d11c1dd001f9126202b40def77e143d7fa8bc
silan.wu <silan.wu@liulishuo.com>
2021-01-29 21:23:35 +0800

    优化

--- demo_three.js
+++ demo_three.js
@@ -1,56 +1,40 @@
-var canvas = document.getElementById('canvas')
-var height = 400
-var width = 400
-var renderer = new THREE.WebGLRenderer({ canvas: canvas })
-var scene = new THREE.Scene()
-var camera = new THREE.OrthographicCamera(-width / 2, width / 2, -height / 2, height / 2, -1000, 1000)
+const canvas = document.getElementById('canvas')
+const height = 400
+const width = 400
+const renderer = new THREE.WebGLRenderer({ canvas: canvas })
+const scene = new THREE.Scene()
+const camera = new THREE.OrthographicCamera(-width / 2, width / 2, height / 2,-height / 2, -100, 100)
 renderer.setSize(400, 400)
 
-var x = -50, y = -100;
-
-var heartShape = new THREE.Shape();
-
-heartShape.moveTo(x, y);
-heartShape.bezierCurveTo(x + 50, y + 50, x + 40, y, x, y);//绘制一条三次贝塞尔曲线
-heartShape.bezierCurveTo(x - 60, y, x - 60, y + 70, x - 60, y + 70);
-heartShape.bezierCurveTo(x - 60, y + 110, x - 30, y + 154, x + 50, y + 190);
-heartShape.bezierCurveTo(x + 120, y + 154, x + 160, y + 110, x + 160, y + 70);
-heartShape.bezierCurveTo(x + 160, y + 70, x + 160, y, x + 100, y);
-heartShape.bezierCurveTo(x + 70, y, x + 50, y + 50, x + 50, y + 50);
-
-// var geometry=new THREE.ShapeGeometry(heartShape);
-var geometry = new THREE.TorusKnotGeometry(50, 10, 100, 16);
-var material = new THREE.MeshBasicMaterial({
+const geometry = new THREE.BoxGeometry(100, 100, 100);
+const material = new THREE.MeshBasicMaterial({
     color: 0xff0000,
     side: THREE.DoubleSide
 
 });
-var mesh = new THREE.Mesh(geometry, material);
-mesh.position.x = 0
-mesh.position.y = 0
-mesh.position.z = 1
+const mesh = new THREE.Mesh(geometry, material);
+mesh.position.set(0, 0, 1)
 scene.add(mesh);
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 0
-camera.lookAt(new THREE.Vector3(0, 0, 1))
-
+camera.position.set(-10, 10, 10)
+camera.lookAt(new THREE.Vector3(0, 0, 0))
+const axesHelper = new THREE.AxesHelper(200);
+axesHelper.position.set(0, 0, 0.5)
+scene.add(axesHelper);
 renderer.render(scene, camera)
-var currentAngle = 0
-var last = Date.now()
+let currentAngle = 0
+let last = Date.now()
 console.log(last)
 function animate() {
-    var now = Date.now()//当前帧时间戳
-    var duration = now - last
+    const now = Date.now()//当前帧时间戳
+    const duration = now - last
     last = now
     currentAngle += duration / 1000 * Math.PI
-    // console.log(duration, currentAngle)
 }
 function render() {
     animate()
-    mesh.rotation.set(currentAngle, currentAngle, currentAngle)//绕Y轴转动
+    mesh.rotation.set(currentAngle, currentAngle, currentAngle)
     renderer.render(scene, camera)
     requestAnimationFrame(render)
 }
-renderer.setClearColor(new THREE.Color(0xE8DAEF, 1))
+renderer.setClearColor(new THREE.Color(0, 0, 0, 1))
 render()
